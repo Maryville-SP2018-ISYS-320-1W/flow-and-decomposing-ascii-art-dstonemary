120f36a91db5fceacb9154a4ecc33317c40b401d
dstonemary <37548808+dstonemary@users.noreply.github.com>
2018-03-25 23:35:07 +0530

    Week1 Decomposing Ascii Art

---
+++ src/RepeatedMessages.java
@@ -0,0 +1,13 @@
+
+public class RepeatedMessages {
+
+	
+	public static void main(String[] args) {
+		for(int i =1; i<=1000; i++){
+			System.out.println("Milhouse did not test cootie positive.");
+		}
+			
+
+	}
+
+}
