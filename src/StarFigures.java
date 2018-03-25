cda2f4a175029b81e210155bb8175a18660b6baf
dstonemary <37548808+dstonemary@users.noreply.github.com>
2018-03-25 23:43:12 +0530

    Week 1 Flow & Decompose Ascii Art

--- src/StarFigures.java
+++ src/StarFigures.java
@@ -1,16 +1,78 @@
 /*
   	ISYS 320
-  	Name(s):
-  	Date: 
+  	Name(s): Derek Stone
+  	Date:    March-25-2018 
 */
 
-// 4. Your pseudocode algorithm for how to break down the figure
+// 4. Five start together in two lines.
+//	  Combination of single and double start in three different lines.
+//    Single start in three lines
+//    Two empty line which keeps space between three different pattern.
+/*
+ * OUTPUT : 
+  *****
+  *****
+   * *
+    *
+   * *
 
+
+  *****
+  *****
+   * *
+    *
+   * *
+  *****
+  *****
+
+
+    *
+    *
+    *
+  *****
+  *****
+   * *
+    *
+   * *
+ */
 
 public class StarFigures {
 
 	public static void main(String[] args) {
-
+		fiveStartPatter();
+		oneTwoStarPatter();
+		emptyLine();
+		
+		fiveStartPatter();
+		oneTwoStarPatter();
+		fiveStartPatter();
+		emptyLine();
+		
+		singleStarPatter();
+		fiveStartPatter();
+		oneTwoStarPatter();	
+		
+	}
+	
+	public static void fiveStartPatter(){
+		System.out.println("  *****");
+		System.out.println("  *****");
+	}
+	
+	public static void oneTwoStarPatter(){
+		System.out.println("   * *");
+		System.out.println("    *");
+		System.out.println("   * *");
+	}
+	
+	public static void singleStarPatter(){
+		for(int i=0;i<3;i++){
+			System.out.println("    *");	
+		}
+	}
+	public static void emptyLine(){
+		System.out.println();
+		System.out.println();
 	}
 
 }
--- src/Tricky.java
+++ src/Tricky.java
@@ -1,17 +1,36 @@
 /*
   	ISYS 320
-  	Name(s):
-  	Date: 
+  	Name(s): Derek Stone
+  	Date:    March-25-2018
 */
 
-// 1. Your predicted output 
- 
+/* 1. Your predicted output 
+ 	This is message1.
+ 	This is message2.
+ 	This is message1.
+ 	Done with message 2.
+ 	Done with main.
+ */
 
 public class Tricky {
-
+    public static void main(String[] args) {
+        message1();
+        message2();
+        System.out.println("Done with main.");
+    }
+    
+    public static void message1() {
+        System.out.println("This is message1.");
+    }
+    
+    public static void message2() {
+        System.out.println("This is message2.");
+        message1();
+        System.out.println("Done with message 2.");
+    }
 }
 
-// 3. Were you correct? Explain any differences
+// 3. All predications are correct as java execute in sequence of statements.
  
  
 
